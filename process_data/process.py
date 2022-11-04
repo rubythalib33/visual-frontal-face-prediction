456cc804fe54446271dea43ed0ad1fc49095e577
rubythalib33 <rubyabdullah14@gmail.com>
2022-11-04 13:46:26 +0700

    update

--- process_data/process.py
+++ process_data/process.py
@@ -1,5 +1,6 @@
 import cv2
 import numpy as np
+from cvzone.SelfiSegmentationModule import SelfiSegmentation
 
 video_front = '../video/front30.mp4'
 video_side = '../video/side30.mp4'
@@ -18,10 +19,15 @@ def resize(image, ratio):
 
     return cv2.resize(image, (w,h))
 
+segmentor = SelfiSegmentation()
+
 
 while True:
     _, frame_f = vid_f.read()
     _, frame_s = vid_s.read()
+
+    frame_f = segmentor.removeBG(frame_f, (0,0,0),0.6)
+    frame_s = segmentor.removeBG(frame_s, (0,0,0), 0.6)
 
     result = np.vstack([frame_f,frame_s])
     result = resize(result, 0.25)
